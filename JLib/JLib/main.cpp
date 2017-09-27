7dd9c27eaa563484e7443979288030494a019835
Young John <JohnYoung404@outlook.com>
2017-09-28 02:26:06 +0800

    revert

--- JLib/JLib/main.cpp
+++ JLib/JLib/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 #include "jLib.h"
 
 // Author : John Young
@@ -6,23 +7,35 @@
 // Date : [9/24/2017]
 
 int main() {
-
-    //jMat_4<double> j = {
-    //    1, 2 ,3, 4,
-    //    2, 2, 3, 4,
-    //    3, 2, 3, 4,
-    //    4, 2, 3, 4
-    //};
-    //
-    //jMat_4<double> k = {
-    //    1, 2, 3, 4,
-    //    1, 2, 3, 4,
-    //    1, 2, 3, 4,
-    //    1, 2, 3, 4
-    //};
-    //
-    //std::cout << jMat_4<double>::Zero() * j + k * jMat_4<double>::I();
-    //
-    //getchar();
+    std::vector<float> a;
+    a.resize(100);
+    float b[100];
+    for (int i = 0; i < 100; ++i)
+    {
+        a[i] = (float)rand() / (i + 1);
+        b[i] = a[i];
+    }
+    float result = 0;
+    {
+        JLOG();
+        for (volatile int i = 0; i < 100000000; ++i)
+        {
+            sumOfArray<100>(a.begin());
+        }
+    }
+    std::cout << sumOfArray<100>(a.begin()) << std::endl;
+    {
+        JLOG();
+        for (volatile int i = 0; i < 100000000; ++i)
+        {
+            result = 0;
+            for (int j = 0; j < 100; ++j)
+            {
+                result = result + a[j];
+            }
+        }
+    }
+    //std::cout << result << std::endl;
+    getchar();
 }
 
