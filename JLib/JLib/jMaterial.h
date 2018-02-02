9d5f22568cdf9bbc2fbb1c8464b722d0aac7ac63
JohnYoung <zhengyang13@mails.tsinghua.edu.cn>
2018-02-02 19:07:58 +0800

    Update jMaterial.

--- JLib/JLib/jMaterial.h
+++ JLib/JLib/jMaterial.h
@@ -1,10 +1,19 @@
 #pragma once
+#include "jVector.h"
 
 namespace jLib {
 namespace jGraphic {
 
 class jMaterial
 {
+    jfloat ambient[3];
+    jfloat diffuse[3];
+    jfloat specular[3];
+    jfloat transmittance[3];    //Í¸ÉäÂÊ
+    jfloat emission[3];
+    jfloat shininess;
+    jfloat ior;                 //index of refraction ÕÛÉäÂÊ
+    jfloat dissolve;            //1 == opaque; 0 == fully transparent
 };
 
 }}
