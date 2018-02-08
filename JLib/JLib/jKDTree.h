945267c96e59fadf3b8044530a3d49ca3a9de3e4
JohnYoung <zhengyang13@mails.tsinghua.edu.cn>
2018-02-08 15:01:09 +0800

    Finish jKDTree.

--- JLib/JLib/jKDTree.h
+++ JLib/JLib/jKDTree.h
@@ -4,13 +4,22 @@
 #include "jRay.h"
 #include "jVector.h"
 #include "jAABBox.h"
+#include "jTriangle.h"
 
 namespace jLib {
 namespace jGraphic {
 
 class jKDNode
 {
+public:
+    std::shared_ptr<jKDNode> build(std::vector<std::shared_ptr<jTriangle>> &tris, int depth);
+    bool hit(std::shared_ptr<jKDNode> node, const jRay &ray, jfloat &t, jfloat &tmin, jVec3f &norm, jVec3f &color);
 
+    jAABBox box;
+    std::shared_ptr<jKDNode> left;
+    std::shared_ptr<jKDNode> right;
+    std::vector<std::shared_ptr<jTriangle>> triangles;
+    bool leaf;
 };
 
 }}
