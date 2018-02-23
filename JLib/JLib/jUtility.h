eac2ff010c215c4ba8372e9c9b45c4577f073a1a
JohnYoung <zhengyang13@mails.tsinghua.edu.cn>
2018-02-23 12:10:33 +0800

    Add forceinline and deprecated keyword

--- JLib/JLib/jUtility.h
+++ JLib/JLib/jUtility.h
@@ -1,4 +1,16 @@
 #pragma once
+
+#ifdef _MSC_VER
+#define jforceinline __forceinline
+#endif
+
+#ifdef __GNUC__
+#define jforceinline inline __attribute__((always_inline))
+#endif
+
+#define jDefaultDeprecatedMsg "Function is Deprecated. Contact JohnYoung404@outlook.com for more info."
+#define jDeprecated __declspec(deprecated(jDefaultDeprecatedMsg))
+
 #include <algorithm>
 
 namespace jLib
