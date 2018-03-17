d92d24a7845759f6c6f9fd524125adcfa8d3166e
JohnYoung <zhengyang13@mails.tsinghua.edu.cn>
2018-03-17 19:42:03 +0800

    Created.

---
+++ JLib/jRayTracing/include/jVec3.h
@@ -0,0 +1,17 @@
+#pragma once
+
+// Author : John Young
+// Contact : JohnYoung404@outlook.com
+// Date : [3/17/2018]
+// Description : Vector contains 3 elements.
+
+namespace jRayTracing
+{
+
+template<typename Type>
+class jVec3
+{
+    jVec3(Type x = 0, Type y = 0, Type z = 0);
+};
+
+}
