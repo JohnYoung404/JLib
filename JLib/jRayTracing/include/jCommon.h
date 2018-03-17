db332c0b8eedcc79ac20d97376a8c03d918280c5
Young John <JohnYoung404@outlook.com>
2018-03-18 02:22:41 +0800

    Add jCommon

---
+++ JLib/jRayTracing/include/jCommon.h
@@ -0,0 +1,10 @@
+#pragma once
+
+// Author : John Young
+// Contact : JohnYoung404@outlook.com
+// Date : [3/18/2018]
+// Description : commonly included header.
+
+#include "jdef.h"
+#include "jfloat.h"
+#include "jDirectXMath.h"
