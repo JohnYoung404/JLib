aec8bb08471dc41830f714faadb11d22da7a4081
JohnYoung <zhengyang13@mails.tsinghua.edu.cn>
2018-03-17 18:36:46 +0800

    Add main.cpp

---
+++ JLib/jRayTracing/source/main.cpp
@@ -0,0 +1,12 @@
+// Author : John Young
+// Contact : JohnYoung404@outlook.com
+// Date : [3/17/2018]
+// Description : program entrance.
+
+#include <iostream>
+
+int main(int argc, char** argv)
+{
+    getchar();
+    return 0;
+}
